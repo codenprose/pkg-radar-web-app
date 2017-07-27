bb51594505415759cb87ff96aacbd095348de026
dkh215 <danielkhunter@gmail.com>
2017-07-27 07:48:00 -0400

    Update styles for Package Index and Package Detail components

--- src/components/Packages/_PackageDetail.js
+++ src/components/Packages/_PackageDetail.js
@@ -1,171 +1,185 @@
-import React, { Component } from 'react'
-import { graphql, compose } from 'react-apollo'
-import Grid from 'material-ui/Grid'
-import Typography from 'material-ui/Typography'
-import Card, { CardHeader, CardContent, CardActions } from 'material-ui/Card'
-import { Link } from 'react-router-dom'
-import Tabs, { Tab } from 'material-ui/Tabs'
-import Button from 'material-ui/Button'
-import SwipeableViews from 'react-swipeable-views'
-import Humanize from 'humanize-plus'
-import marked from 'marked'
-import Chip from 'material-ui/Chip'
+import React, { Component } from "react";
+import { graphql, compose } from "react-apollo";
+import Grid from "material-ui/Grid";
+import Typography from "material-ui/Typography";
+import Card, { CardHeader, CardContent, CardActions } from "material-ui/Card";
+import { Link } from "react-router-dom";
+import Tabs, { Tab } from "material-ui/Tabs";
+import Button from "material-ui/Button";
+import SwipeableViews from "react-swipeable-views";
+import Humanize from "humanize-plus";
+import marked from "marked";
+import Chip from "material-ui/Chip";
 import Dialog, {
   DialogActions,
   DialogContent,
-  DialogTitle,
-} from 'material-ui/Dialog'
-import TextField from 'material-ui/TextField'
-import moment from 'moment'
+  DialogTitle
+} from "material-ui/Dialog";
+import TextField from "material-ui/TextField";
+import moment from "moment";
 
-import 'github-markdown-css/github-markdown.css'
+import "github-markdown-css/github-markdown.css";
 
-import fetchPackage from '../../queries/fetchPackage'
-import updatePackageRecommendations from '../../mutations/updatePackageRecommendations'
+import fetchPackage from "../../queries/fetchPackage";
+import updatePackageRecommendations from "../../mutations/updatePackageRecommendations";
 
 const TabContainer = props =>
-  <div style={{ padding: '24px' }}>
+  <div style={{ marginTop: "20px" }}>
     {props.children}
-  </div>
+  </div>;
 
 class PackageDetail extends Component {
   state = {
     index: 0,
     isModalOpen: false,
-    recommendationSearch: '',
+    recommendationSearch: "",
     recommendationSelection: {}
-  }
+  };
 
   handleMainContentTabChange = (event, index) => {
-    this.setState({ index })
-  }
+    this.setState({ index });
+  };
 
-  handleMainContentChangeIndex = (index) => {
-    this.setState({ index })
-  }
+  handleMainContentChangeIndex = index => {
+    this.setState({ index });
+  };
 
   _handleModalOpen = () => {
-    this.setState({ isModalOpen: true })
-  }
+    this.setState({ isModalOpen: true });
+  };
 
   _handleModalClose = () => {
-    this.setState({ isModalOpen: false })
-  }
+    this.setState({ isModalOpen: false });
+  };
 
-  _handleRecommendationSearch = (pkg) => {
-    this.setState({ recommendationSearch: pkg })
-  }
+  _handleRecommendationSearch = pkg => {
+    this.setState({ recommendationSearch: pkg });
+  };
 
   _handleAddRecommendation = () => {
     // TODO: check if recommendation already exists
     const variables = {
       recommendations: [
-        ...this.props.data.Package.recommendations, 
+        ...this.props.data.Package.recommendations,
         this.state.recommendationSelection
       ]
-    }
+    };
 
-    console.log('adding recommendation')
+    console.log("adding recommendation");
 
-    this.props.updatePackageRecommendations({ variables })
-      .then((response) => {
-        console.log(response)
+    this.props
+      .updatePackageRecommendations({ variables })
+      .then(response => {
+        console.log(response);
       })
-      .catch((err) => {
-        console.error('error creating package', err.message)
-      })
-  }
+      .catch(err => {
+        console.error("error creating package", err.message);
+      });
+  };
 
   _renderRecommendations = () => {
-    const { Package } = this.props.data
-    return Package.recommendations.map(({ name, avatar, stars, description }) => {
-      return (
+    const { Package } = this.props.data;
+    return Package.recommendations.map(
+      ({ name, avatar, stars, description }) => {
+        return (
           <Grid item xs={4} key={name}>
-            <Card style={{ marginBottom: '15px' }}>
+            <Card 
+              style={{ 
+                marginBottom: "15px",
+                boxShadow: "5px 5px 25px 0px rgba(46,61,73,0.2)"
+              }}
+            >
               <CardHeader
                 avatar={
                   <img
                     alt={`${name}-logo`}
-                    style={{ height: '40px' }}
+                    style={{ height: "40px" }}
                     src={avatar}
                   />
                 }
                 title={name}
-                subheader="Stars: 32,197"
+                subheader={`Stars: ${stars}`}
               />
-              <CardContent style={{ padding: '0 16px' }}>
+              <CardContent style={{ padding: "0 16px" }}>
                 <Typography type="body1" component="p">
                   {description}
                 </Typography>
               </CardContent>
               <CardActions>
-                <Link to={`/package/${name}`} className='no-underline'>
+                <Link to={`/package/${name}`} className="no-underline">
                   <Button dense>View Package</Button>
                 </Link>
               </CardActions>
             </Card>
           </Grid>
-      )
-    })
-  }
+        );
+      }
+    );
+  };
 
   render() {
-    const { data, history } = this.props
-    if (data.loading) return <div></div>
+    const { data, history } = this.props;
+    if (data.loading) return <div />;
 
-    let readmeHtml = '<div>No Data Available</div>'
-    let changelogHtml = '<div>No Data Available</div>'
-    
+    let readmeHtml = "<div>No Data Available</div>";
+    let changelogHtml = "<div>No Data Available</div>";
+
     if (data.Package.lastRelease) {
-      changelogHtml = marked(data.Package.lastRelease.description)
+      changelogHtml = marked(data.Package.lastRelease.description);
     }
 
     if (data.Package.changelog) {
-      changelogHtml = marked(data.Package.changelog)
+      changelogHtml = marked(data.Package.changelog);
     }
 
     if (data.Package.readme) {
-      readmeHtml = marked(data.Package.readme)
+      readmeHtml = marked(data.Package.readme);
     }
 
     const styles = {
       card: {
-        marginBottom: '15px',
-        boxShadow: '5px 5px 25px 0px rgba(46,61,73,0.2)'
-      },
-    }
+        marginBottom: "15px",
+        boxShadow: "5px 5px 25px 0px rgba(46,61,73,0.2)"
+      }
+    };
 
     return (
       <div>
         <Grid container>
-
           <Grid item xs={3}>
+
             {/* Package Title */}
-             <Card style={styles.card}>
+            <Card style={styles.card}>
               <CardHeader
                 avatar={
-                  <img 
+                  <img
                     alt={`${data.Package.name}-logo`}
-                    style={{ height: '42px' }} 
-                    src={data.Package.avatar} 
+                    style={{ height: "42px" }}
+                    src={data.Package.avatar}
                   />
                 }
                 title={`${data.Package.owner}/${data.Package.name}`}
                 subheader={data.Package.primaryLanguage.name}
               />
               <CardActions>
-                {
-                  data.Package.homepageUrl &&
-                  <Link to={data.Package.homepageUrl} target='_blank' className='no-underline'>
+                {data.Package.homepageUrl &&
+                  <Link
+                    to={data.Package.homepageUrl}
+                    target="_blank"
+                    className="no-underline"
+                  >
                     <Button dense>Website</Button>
-                  </Link>
-                }
-                <Link to={data.Package.repoUrl} target='_blank' className='no-underline'>
+                  </Link>}
+                <Link
+                  to={data.Package.repoUrl}
+                  target="_blank"
+                  className="no-underline"
+                >
                   <Button dense>Repo</Button>
                 </Link>
               </CardActions>
-            </Card> 
-            
+            </Card>
+
             {/* Tags */}
             <Card style={styles.card}>
               <CardContent style={{ paddingBottom: 0 }}>
@@ -209,11 +223,17 @@ class PackageDetail extends Component {
                   Last Commit
                 </Typography>
                 <Typography type="body1">
-                  {moment(data.Package.lastCommit.author.date).format("MMMM Do, YYYY")}
+                  {moment(data.Package.lastCommit.author.date).format(
+                    "MMMM Do, YYYY"
+                  )}
                 </Typography>
               </CardContent>
               <CardActions>
-                <Link to={data.Package.lastCommit.commitUrl} target='_blank' className='no-underline'>
+                <Link
+                  to={data.Package.lastCommit.commitUrl}
+                  target="_blank"
+                  className="no-underline"
+                >
                   <Button dense>View Commit</Button>
                 </Link>
               </CardActions>
@@ -276,9 +296,7 @@ class PackageDetail extends Component {
                 <Typography type="title" component="h2">
                   Contributors
                 </Typography>
-                <Typography type="body1">
-                  Add Contributors
-                </Typography>
+                <Typography type="body1">Add Contributors</Typography>
               </CardContent>
               <CardActions>
                 <Button dense>View Contributors</Button>
@@ -299,43 +317,11 @@ class PackageDetail extends Component {
                 <Button dense>View License</Button>
               </CardActions>
             </Card>
-
           </Grid>
 
           {/* Tabs */}
-          <Grid item xs={9}>
+          <Grid item xs={9} style={{ paddingLeft: '20px' }}>
             <Grid container>
-              {/* <Grid item xs={12}>
-                <div style={{ marginBottom: '5px' }}>
-                  <Typography 
-                    style={{ display: 'inline-block', color: 'rgb(1%, 40%, 84%)' }} 
-                    type="headline"
-                  >
-                    {data.Package.owner} / {data.Package.name}
-                  </Typography>
-                </div>
-                <Typography style={{ marginBottom: '15px' }} type="subheading">
-                  {data.Package.description}
-                </Typography>
-                {
-                  data.Package.tags.map(({text}) => {
-                    return (
-                      <Link to={`/search?=${text}`} key={text}>
-                        <Chip 
-                          label={text} 
-                          style={{
-                            display: 'inline-block', 
-                            margin: '0 10px 10px 0',
-                            cursor: 'pointer',
-                            borderRadius: 0
-                          }} 
-                        />
-                      </Link>
-                    )
-                  })
-                }
-                <Button dense raised>Update Tags</Button>
-              </Grid> */}
               <Grid style={{ paddingTop: 0 }} item xs={9}>
                 <Tabs
                   index={this.state.index}
@@ -351,29 +337,49 @@ class PackageDetail extends Component {
                 </Tabs>
               </Grid>
               <Grid item xs={3}>
-                <Button raised style={{ marginRight: '10px' }}>Save To Board</Button>
+                <Button raised style={{ marginRight: "10px" }}>
+                  Save To Board
+                </Button>
                 {/* <Button raised>Monitor</Button> */}
               </Grid>
             </Grid>
-            <SwipeableViews index={this.state.index} onChangeIndex={this.handleMainContentChangeIndex}>
+            <SwipeableViews
+              index={this.state.index}
+              onChangeIndex={this.handleMainContentChangeIndex}
+            >
               <TabContainer>
-                <div className='markdown-body' dangerouslySetInnerHTML={{__html: readmeHtml}} />
+                <div
+                  className="markdown-body"
+                  dangerouslySetInnerHTML={{ __html: readmeHtml }}
+                />
               </TabContainer>
               <TabContainer>
-                {
-                  data.Package.lastRelease &&
+                {data.Package.lastRelease &&
                   <div>
-                    <Typography style={{ marginBottom: '10px' }} type="display1" component="h2">
-                      {data.Package.lastRelease.name} 
+                    <Typography
+                      style={{ marginBottom: "10px" }}
+                      type="display1"
+                      component="h2"
+                    >
+                      {data.Package.lastRelease.name}
                     </Typography>
-                    <Typography style={{ marginBottom: '20px' }} type="body1" component="p">
-                      Published: {moment(data.Package.lastRelease.publishedAt).format("MMMM Do, YYYY")}
+                    <Typography
+                      style={{ marginBottom: "20px" }}
+                      type="body1"
+                      component="p"
+                    >
+                      Published:{" "}
+                      {moment(data.Package.lastRelease.publishedAt).format(
+                        "MMMM Do, YYYY"
+                      )}
                     </Typography>
-                  </div>
-                }
-                <div className='markdown-body' dangerouslySetInnerHTML={{__html: changelogHtml}} />
+                  </div>}
+                <div
+                  className="markdown-body"
+                  dangerouslySetInnerHTML={{ __html: changelogHtml }}
+                />
               </TabContainer>
-              
+
               {/* Recommendations */}
               <TabContainer>
                 <Grid container>
@@ -381,14 +387,14 @@ class PackageDetail extends Component {
                     data.Package.recommendations &&
                     this._renderRecommendations()
                   }
-                  
+
                   {/* Add Recommendation */}
                   <Grid item xs={4}>
-                    <Grid 
-                      container 
-                      align='center' 
-                      justify='center'
-                      style={{ height: '100%' }}
+                    <Grid
+                      container
+                      align="center"
+                      justify="center"
+                      style={{ height: "100%" }}
                     >
                       <Grid item>
                         <Button raised onClick={() => this._handleModalOpen()}>
@@ -400,56 +406,58 @@ class PackageDetail extends Component {
                   {/* /Add Recommendation */}
                 </Grid>
               </TabContainer>
-              
-              <TabContainer>{'Analytics'}</TabContainer>
+
+              <TabContainer>
+                {"Analytics"}
+              </TabContainer>
             </SwipeableViews>
           </Grid>
-          
         </Grid>
 
-        <Dialog 
-          style={{ width: '100%' }} 
+        <Dialog
+          style={{ width: "100%" }}
           open={this.state.isModalOpen}
           onRequestClose={this._handleModalClose}
         >
-           <DialogTitle>Add New Package</DialogTitle>
-           <DialogContent style={{ width: '500px' }}>
-             <TextField
-               autoFocus
-               style={{ width: '100%' }}
-               value={this.state.recommendationSearch}
-               placeholder='Search for Package'
-               onChange={(e) => this._handleRecommendationSearch(e.target.value)}
-             />
-           </DialogContent>
-           <DialogActions>
-             <Button
-               className='mr3'
-               onTouchTap={this._handleModalClose}
-             >
-               Cancel
+          <DialogTitle>Add New Package</DialogTitle>
+          <DialogContent style={{ width: "500px" }}>
+            <TextField
+              autoFocus
+              style={{ width: "100%" }}
+              value={this.state.recommendationSearch}
+              placeholder="Search for Package"
+              onChange={e => this._handleRecommendationSearch(e.target.value)}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button className="mr3" onTouchTap={this._handleModalClose}>
+              Cancel
             </Button>
-             <Button
-               raised
-               color="primary"
-               onTouchTap={this._handleAddRecommendation}
-             >
-               Submit
+            <Button
+              raised
+              color="primary"
+              onTouchTap={this._handleAddRecommendation}
+            >
+              Submit
             </Button>
-           </DialogActions>
-         </Dialog>
+          </DialogActions>
+        </Dialog>
       </div>
-    )
+    );
   }
 }
 
 const fetchPackageOptions = {
-  options: (props) => { return { 
-    variables: { name: props.match.params.name } } 
+  options: props => {
+    return {
+      variables: { name: props.match.params.name }
+    };
   }
-}
+};
 
 export default compose(
   graphql(fetchPackage, fetchPackageOptions),
-  graphql(updatePackageRecommendations, { name: 'updatePackageRecommendations'})
-)(PackageDetail)
+  graphql(updatePackageRecommendations, {
+    name: "updatePackageRecommendations"
+  })
+)(PackageDetail);
--- src/components/Packages/_PackageIndex.js
+++ src/components/Packages/_PackageIndex.js
@@ -1,54 +1,46 @@
 import React, { Component } from 'react'
 import { graphql, compose } from 'react-apollo'
 import Grid from "material-ui/Grid";
-// import ViewModuleIcon from 'material-ui-icons/ViewModule';
-// import ViewListIcon from 'material-ui-icons/ViewList';
 
-// import PackageIndexCategories from './_PackageIndexCategories'
-import PackageIndexTable from './_PackageIndexTable'
+import Tabs, { Tab } from 'material-ui/Tabs'
+
 import PackageCard from './_PackageCard'
 import AllPackagesQuery from '../../queries/fetchAllPackages'
 
 import javascript from "../../images/javascript.svg"
-import python from "../../images/python.svg"
-import java from "../../images/java.svg"
-import go from "../../images/go.svg"
-import swift from "../../images/swift.svg"
-import c from "../../images/c.svg"
-import cPlusPlus from "../../images/cPlusPlus.svg"
-import cSharp from "../../images/cSharp.svg"
-import php from "../../images/php.svg"
-import ruby from "../../images/ruby.svg"
+// import python from "../../images/python.svg"
+// import java from "../../images/java.svg"
+// import go from "../../images/go.svg"
+// import swift from "../../images/swift.svg"
+// import c from "../../images/c.svg"
+// import cPlusPlus from "../../images/cPlusPlus.svg"
+// import cSharp from "../../images/cSharp.svg"
+// import php from "../../images/php.svg"
+// import ruby from "../../images/ruby.svg"
 
 
 class PackageIndex extends Component {
-  state = {
-    view: "cards"
-  };
-
-  _renderPackageLayout = () => {
-    const { view } = this.state;
-    const { allPackages } = this.props.data;
-
-    if (view === "table") {
-      return <PackageIndexTable packages={allPackages} />;
-    } else {
-      return allPackages.map(pkg => {
-        return (
-          <Grid key={pkg.id} item xs={12} md={4} lg={3}>
-            <PackageCard pkg={pkg} />
-          </Grid>
-        );
-      });
-    }
-  };
-
   _renderCategory = (data, imgSrc) => {
     return (
       <div>
         <Grid container direction="row">
           <Grid item>
-            <img src={imgSrc} alt="logo" />
+            <img 
+              src={imgSrc} 
+              alt="logo"
+              style={{ marginRight: '20px' }}
+            />
+            <Tabs
+              index={0}
+              onChange={() => console.log('tab change')}
+              indicatorColor="primary"
+              textColor="primary"
+              fullWidth
+              style={{ display: 'inline-block' }}
+            >
+              <Tab label="Top" />
+              <Tab label="Latest" />
+            </Tabs>
           </Grid>
         </Grid>
         <Grid container direction="row">
@@ -77,62 +69,38 @@ class PackageIndex extends Component {
     }
 
     return (
-      <div>
-        {/* <div style={{ marginBottom: '30px' }}>
-          <h3 className='ma0'>Top Packages</h3>
-           <div className='fr mb2'>
-            <ViewModuleIcon 
-              onClick={() => this.setState({ view: 'cards' })}
-              style={{
-                height: '32px',
-                width: '32px',
-                verticalAlign: 'top',
-                cursor: 'pointer'
-              }} 
-            />
-            <ViewListIcon 
-              onClick={() => this.setState({ view: 'table' })}
-              style={{
-                height: '30px',
-                width: '30px',
-                cursor: 'pointer'
-              }}
-            />
-          </div> 
-        </div> */}
-        <Grid container direction="row">
-          <Grid item xs={12} style={styles.category}>
-            {this._renderCategory(data.allPackages, javascript)}
-          </Grid>
-          <Grid item xs={12} style={styles.category}>
-            {this._renderCategory(data.allPackages, python)}
-          </Grid>
-          <Grid item xs={12} style={styles.category}>
-            {this._renderCategory(data.allPackages, java)}
-          </Grid>
-          <Grid item xs={12} style={styles.category}>
-            {this._renderCategory(data.allPackages, go)}
-          </Grid>
-          <Grid item xs={12} style={styles.category}>
-            {this._renderCategory(data.allPackages, swift)}
-          </Grid>
-          <Grid item xs={12} style={styles.category}>
-            {this._renderCategory(data.allPackages, c)}
-          </Grid>
-          <Grid item xs={12} style={styles.category}>
-            {this._renderCategory(data.allPackages, cPlusPlus)}
-          </Grid>
-          <Grid item xs={12} style={styles.category}>
-            {this._renderCategory(data.allPackages, cSharp)}
-          </Grid>
-          <Grid item xs={12} style={styles.category}>
-            {this._renderCategory(data.allPackages, php)}
-          </Grid>
-          <Grid item xs={12} style={styles.category}>
-            {this._renderCategory(data.allPackages, ruby)}
-          </Grid>
+      <Grid container direction="row">
+        <Grid item xs={12} style={styles.category}>
+          {this._renderCategory(data.allPackages, javascript)}
         </Grid>
-      </div>
+        {/* <Grid item xs={12} style={styles.category}>
+          {this._renderCategory(data.allPackages, python)}
+        </Grid>
+        <Grid item xs={12} style={styles.category}>
+          {this._renderCategory(data.allPackages, java)}
+        </Grid>
+        <Grid item xs={12} style={styles.category}>
+          {this._renderCategory(data.allPackages, go)}
+        </Grid>
+        <Grid item xs={12} style={styles.category}>
+          {this._renderCategory(data.allPackages, swift)}
+        </Grid>
+        <Grid item xs={12} style={styles.category}>
+          {this._renderCategory(data.allPackages, c)}
+        </Grid>
+        <Grid item xs={12} style={styles.category}>
+          {this._renderCategory(data.allPackages, cPlusPlus)}
+        </Grid>
+        <Grid item xs={12} style={styles.category}>
+          {this._renderCategory(data.allPackages, cSharp)}
+        </Grid>
+        <Grid item xs={12} style={styles.category}>
+          {this._renderCategory(data.allPackages, php)}
+        </Grid>
+        <Grid item xs={12} style={styles.category}>
+          {this._renderCategory(data.allPackages, ruby)}
+        </Grid> */}
+      </Grid>
     );
   }
 }
