8a1fa358527f94101392b71defb254f558c16b8c
dkh215 <danielkhunter@gmail.com>
2017-09-17 19:31:27 -0400

    Update webpack prod config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -84,7 +84,7 @@ module.exports = {
     // https://github.com/facebookincubator/create-react-app/issues/290
     extensions: ['.js', '.json', '.jsx'],
     alias: {
-      
+
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
@@ -114,7 +114,7 @@ module.exports = {
           {
             options: {
               formatter: eslintFormatter,
-              
+
             },
             loader: require.resolve('eslint-loader'),
           },
@@ -160,7 +160,7 @@ module.exports = {
         test: /\.(js|jsx)$/,
         include: paths.appSrc,
         loader: require.resolve('babel-loader'),
-        
+
       },
       // The notation here is somewhat confusing.
       // "postcss" loader applies autoprefixer to our CSS.
@@ -247,8 +247,7 @@ module.exports = {
     // It is absolutely essential that NODE_ENV was set to production here.
     // Otherwise React will be compiled in the very slow development mode.
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': env.stringified,
-      'process.env.GRAPHQL_ENDPOINT': JSON.stringify('https://api.graph.cool/simple/v1/cj4de7is0kt0j01722tiovqqa')
+      'process.env.NODE_ENV': 'production',
     }),
     // Minify the code.
     new webpack.optimize.UglifyJsPlugin({
