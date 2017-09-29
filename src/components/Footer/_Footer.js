8344616f3dffa7cbae576e0277bd8adddf1d1e56
dkh215 <danielkhunter@gmail.com>
2017-09-29 15:31:09 -0400

    Add search examples to homepage

--- src/components/Footer/_Footer.js
+++ src/components/Footer/_Footer.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
 // import { Link } from 'react-router-dom'
 import Typography from 'material-ui/Typography';
+import ReactRotatingText from 'react-rotating-text';
 
 class Footer extends Component {
+  state = {
+    searchExamples: ['machine learning', 'data science', 'serverless python', 'frontend javascript frameworks', 'aws']
+  }
+
   render() {
     const styles = {
       containerOuter: {
@@ -15,30 +20,34 @@ class Footer extends Component {
         maxWidth: '1600px',
         margin: '0 auto',
         padding: '15px 20px',
-        textAlign: 'right'
       }
     }
     return (
       <footer style={styles.containerOuter}>
         <div style={styles.containerInner}>
-          <Typography 
-            type="subheading"
-            style={{
-              display: 'inline-block',
-              marginRight: '10px',
-              verticalAlign: 'middle'
-            }}
-          >
-            Made in Philly by
-          </Typography>
-          <a 
-            href="https://twitter.com/danielkhunter" 
-            className="twitter-follow-button no-underline"
-            target='_blank'
-            rel="noopener noreferrer"
-          >
-            @danielkhunter
-          </a>
+          <div style={{ display: 'inline-block', textAlign: 'left', width: '50%' }}>
+            <ReactRotatingText id='pr-search-examples' items={this.state.searchExamples} />
+          </div>
+          <div style={{ display: 'inline-block', textAlign: 'right', width: '50%' }}>
+            <Typography
+              type="subheading"
+              style={{
+                display: 'inline-block',
+                marginRight: '10px',
+                verticalAlign: 'middle'
+              }}
+            >
+              Made in Philly by
+            </Typography>
+            <a
+              href="https://twitter.com/danielkhunter"
+              className="twitter-follow-button no-underline"
+              target='_blank'
+              rel="noopener noreferrer"
+            >
+              @danielkhunter
+            </a>
+          </div>
           {/* <Link className='no-underline black mr4' to='/'>About</Link>
           <Link className='no-underline black mr4' to='/'>Boards</Link> */}
         </div>
