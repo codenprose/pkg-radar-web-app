bff395d45b6ddbb481f37336f8d74cc60df1ff4c
dkh215 <danielkhunter@gmail.com>
2017-10-12 16:24:24 -0400

    Update copy for About page

--- src/components/About/_About.js
+++ src/components/About/_About.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 
 class About extends Component {
+  static defaultProps = {
+    title: '<pkg> radar'
+  }
+
   render() {
+    const { title } = this.props;
+
     return (
-      <div>
-        About
+      <div style={{ maxWidth: '1040px' }}>
+        <p style={{ fontSize: '20px', lineHeight: '1.5' }}>
+          <strong>{title}</strong> is a search and discovery tool for open source software that allows users to
+          organize packages into four meaningful categories: Backlog, Trial, Production, Archive.
+        </p>
       </div>
     )
   }
